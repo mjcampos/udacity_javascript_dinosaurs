befa70477b16805ba65c60c7dc994e04aa9db791
Michael Campos <camposmj89@gmail.com>
2021-05-24 22:54:23 -0700

    First commit for homework

--- app.js
+++ app.js
@@ -1,32 +1,195 @@
+// Create Dino Constructor
+function Dinos() {
+    this.dinos = [
+        {
+            "species": "Triceratops",
+            "weight": 13000,
+            "height": 114,
+            "diet": "herbavor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "First discovered in 1889 by Othniel Charles Marsh"
+        },
+        {
+            "species": "Tyrannosaurus Rex",
+            "weight": 11905,
+            "height": 144,
+            "diet": "carnivor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "The largest known skull measures in at 5 feet long."
+        },
+        {
+            "species": "Anklyosaurus",
+            "weight": 10500,
+            "height": 55,
+            "diet": "herbavor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "Anklyosaurus survived for approximately 135 million years."
+        },
+        {
+            "species": "Brachiosaurus",
+            "weight": 70000,
+            "height": "372",
+            "diet": "herbavor",
+            "where": "North America",
+            "when": "Late Jurasic",
+            "fact": "An asteroid was named 9954 Brachiosaurus in 1991."
+        },
+        {
+            "species": "Stegosaurus",
+            "weight": 11600,
+            "height": 79,
+            "diet": "herbavor",
+            "where": "North America, Europe, Asia",
+            "when": "Late Jurasic to Early Cretaceous",
+            "fact": "The Stegosaurus had between 17 and 22 seperate places and flat spines."
+        },
+        {
+            "species": "Elasmosaurus",
+            "weight": 16000,
+            "height": 59,
+            "diet": "carnivor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "Elasmosaurus was a marine reptile first discovered in Kansas."
+        },
+        {
+            "species": "Pteranodon",
+            "weight": 44,
+            "height": 20,
+            "diet": "carnivor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "fact": "Actually a flying reptile, the Pteranodon is not a dinosaur."
+        },
+        {
+            "species": "Pigeon",
+            "weight": 0.5,
+            "height": 9,
+            "diet": "herbavor",
+            "where": "World Wide",
+            "when": "Holocene",
+            "fact": "All birds are living dinosaurs."
+        }
+    ];
 
-    // Create Dino Constructor
+    this.createDino = function(species, weight, height, diet, where, when, fact) {
+        var newDino = {
+            species,
+            weight,
+            height,
+            diet,
+            where,
+            when,
+            fact
+        }
 
+        this.dinos.push(newDino);
+    }
+}
 
-    // Create Dino Objects
+// Create Dino Objects
+let dinos = new Dinos();
 
+// Create Human Object
+function Human(name, feet, inches, weight, diet) {
+    this.name = name;
+    this.feet = feet;
+    this.inches = inches;
+    this.weight = weight;
+    this.diet = diet;
+}
 
-    // Create Human Object
+var human;
 
-    // Use IIFE to get human data from form
+// Use IIFE to get human data from form
+var btn = document.getElementById('btn');
 
+btn.addEventListener('click', (function() {
+    return function() {
+        var name = document.getElementById('name').value;
+        var feet = document.getElementById('feet').value;
+        var inches = document.getElementById('inches').value;
+        var weight = document.getElementById('weight').value;
+        var diet = document.getElementById('diet').value;
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+        human = new Human(name, feet, inches, weight, diet);
 
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+        var form = document.getElementById('dino-compare');
+        form.remove();
+        main.style.display === "block";
 
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+        var mainArr = [];
+        var t;
 
+        for (var i = 0; i < dinos.dinos.length; i++) {
+            var item = dinos.dinos[i];
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+            // Create grid div
+            var dino = document.createElement("div");
 
-    // Remove form from screen
+            dino.classList.add('grid-item');
+
+            // Add species name with h1 tags and append to dino div
+            var h1 = document.createElement('h3');
+
+            t = document.createTextNode(item.species);
+            h1.appendChild(t);
+            dino.appendChild(h1);
+
+            // Add fact
+            var p = document.createElement('p');
+
+            t = document.createTextNode(item.fact);
+            p.appendChild(t);
+            dino.appendChild(p);
+
+            mainArr.push(dino);
+        }
+
+        // Insert human data randomly in mainArr
+        var rndInt = Math.floor(Math.random() * 8);
+
+        // create grid div
+        var humanItem = document.createElement("div");
+
+        humanItem.classList.add('grid-item');
+
+        // add name
+        var h1 = document.createElement('h3');
+        t = document.createTextNode(human.name);
+        h1.appendChild(t);
+        humanItem.appendChild(h1);
+
+        mainArr.splice(rndInt, 1, humanItem);
+
+        mainArr.forEach(el => {
+            main.appendChild(el);
+        });
+    }
+})());
+
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
+dinos.createDino("Velociraptor", 7, 0, 35, "carnivor", "North America", "Late Cretaceous", "They are smaller than they appear in the movies");
+
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
+dinos.createDino("Indo-Raptor", 14, 2, 70, "carnivor", "Central America", "Modern", "Made up for Jurrasic World: Lost Kingdom");
+
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+dinos.createDino("Indomitus Rex", 23005, 155, "carnivor", "Central America", "Modern", "Created by INGEN for Jurassic World");
+
+// Generate Tiles for each Dino in Array
+
+    // Add tiles to DOM
+
+// Remove form from screen
 
 
 // On button click, prepare and display infographic
+var main = document.getElementById('grid');
+main.style.display === "none";
